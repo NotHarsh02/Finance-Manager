241d462402323f5ca7b63d347ec1a3a5b0013e5c
unknown <harshthegreat2002@gmail.com>
2024-07-21 20:33:35 +0530

    added bulk delete feature

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,32 +1,38 @@
 "use client";
 
-import { Plus } from "lucide-react";
+import { Plus,Loader2} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
 import { DataTable } from "@/components/data-table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { ResponseType, columns } from "./columns";
 
-import { Payment, columns } from "./columns";
-
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed54s",
-    amount: 50,
-    status: "success",
-    email: "a@example.com",
-  },
-];
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
-
+  const accountsQuery=useGetAccounts();
+  const accounts=accountsQuery.data ||[]
+  if(accountsQuery.isLoading){
+    return(
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm"> 
+          <CardHeader>
+          <Skeleton className="h-8 w-48"></Skeleton>
+            </CardHeader>
+            <CardContent>
+              <div className="h-[500px] w-full flex items-center justify-center">
+               <Loader2 className="size-6 text-slate-300 animate-spin"></Loader2>
+              </div>
+            </CardContent>
+          </Card>
+      </div>
+      
+    )
+  }
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -40,7 +46,7 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
             onDelete={() => {}}
             disabled={false}
--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,18 +1,21 @@
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/neon-http";
-import { migrate } from "drizzle-orm/neon-http/migrator";
+// src/migrate.ts
 
-config({ path: ".env.local" });
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
 
 const main = async () => {
   try {
-    await migrate(db, { migrationsFolder: "drizzle" });
+    await migrate(db, { migrationsFolder: 'drizzle' });
+    console.log('Migration completed');
   } catch (error) {
-    console.error("Error during migration", error);
+    console.error('Error during migration:', error);
     process.exit(1);
   }
 };
