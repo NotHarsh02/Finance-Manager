174aad155f97bf4c10bcab2f268229e88010375e
unknown <harshthegreat2002@gmail.com>
2024-09-08 21:49:19 +0530

    implemented accounts edit and delete features

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import {and,eq,inArray } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2";
 import { zValidator } from "@hono/zod-validator";
-import {z} from "zod";
+import {string, z} from "zod";
 import { db } from "@/db/drizzle";
 import { accounts, insertAccountsSchema } from "@/db/schema";
 
@@ -23,7 +23,39 @@ const app = new Hono()
       .from(accounts)
       .where(eq(accounts.userId, auth.userId));
     return c.json({ data });
-  })
+  }).get(
+    "/:id",
+    zValidator("param",z.object({
+      id:z.string().optional(),
+    })),clerkMiddleware(),
+    async (c)=>{
+      const auth = getAuth(c)
+      const {id}=c.req.valid("param");
+      if(!id){
+        return c.json({error:"Missing Id"},400)
+      }
+      if(!auth?.userId){
+        return c.json({error:"Unauthorised"},401)
+      }
+
+      const [data]=await db.
+      select({
+        id:accounts.id,
+        name:accounts.name
+      }).from(accounts).where(
+        and(
+          eq(accounts.userId,auth.userId),
+          eq(accounts.id,id)
+        )
+      )
+      if(!data){
+        return c.json({error:"not found"},404);
+      }
+
+      return c.json({data});
+    }
+   
+  )
   .post(
     "/",
     clerkMiddleware(),
@@ -79,5 +111,79 @@ const app = new Hono()
     }
 
   )
+  .patch("/:id",clerkMiddleware(),zValidator(
+    "param",z.object({
+      id:z.string().optional(),
+    })
+  ),zValidator(
+    "json",
+    insertAccountsSchema.pick({
+      name:true,
+    })
+  ),
+async (c)=>{
+  const auth=getAuth(c);
+  const {id}=c.req.valid("param")
+  const values=c.req.valid("json")
+
+  if(!id){
+    return c.json({error:"Missing Id"},400)
+  }
+  if(!auth?.userId){
+    return c.json({error:"Unauthorized"},401);
+  }
+    const [data]=await db
+    .update(accounts)
+    .set(values)
+    .where(
+      and(
+        eq(accounts.userId,auth.userId),
+        eq(accounts.id,id)
+      ),
+    ).returning();
+  
+    if(!data){
+      return c.json({error:"Data not found"},404)
+    }
+    return c.json({data})
+  }
+
+
+).
+delete("/:id",clerkMiddleware(),zValidator(
+  "param",z.object({
+    id:z.string().optional(),
+  })
+),
+async (c)=>{
+const auth=getAuth(c);
+const {id}=c.req.valid("param")
+
+
+if(!id){
+  return c.json({error:"Missing Id"},400)
+}
+if(!auth?.userId){
+  return c.json({error:"Unauthorized"},401);
+}
+  const [data]=await db
+  .delete(accounts)
+  .where(
+    and(
+      eq(accounts.userId,auth.userId),
+      eq(accounts.id,id)
+    ),
+  ).returning({
+    id:accounts.id
+  });
+
+  if(!data){
+    return c.json({error:"Data not found"},404)
+  }
+  return c.json({data})
+}
+
+)
+
 
 export default app;
